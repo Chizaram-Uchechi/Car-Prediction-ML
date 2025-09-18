41c0d8f9a68355d94b20691ef14cacd393564164
Chizaram Uchechi Egbuchiwe <uzara2005@gmail.com>
2025-09-18 18:11:39 +0100

    Update Carpred.py

--- Carpred.py
+++ Carpred.py
@@ -9,9 +9,9 @@ st.set_page_config(
     menu_items={
         'About': "This app estimates a fair selling price for your car using a machine-learning model."
     },
-    primaryColor = "#008080"
-    backgroundColor = "#0E0D0D"
-    secondaryBackgroundColor = "#912065"
+    primaryColor = #008080
+    backgroundColor = #0E0D0D
+    secondaryBackgroundColor = #912065
     textColor = "#FFFFFF"
     font = "sans serif"
 
@@ -90,6 +90,7 @@ if show_about:
     * The algorithm weighs these features against market trends.
     * You get an evidence-based price estimate before you visit a dealer.
     """)
+
 
 
 
