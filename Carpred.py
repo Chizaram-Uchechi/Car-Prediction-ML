d341de3d498182603ebc22ee7e13a3c185c6bf38
Chizaram Uchechi Egbuchiwe <uzara2005@gmail.com>
2025-09-18 18:43:41 +0100

    Update Carpred.py

--- Carpred.py
+++ Carpred.py
@@ -15,7 +15,8 @@ st.markdown("""
 <style>
 /* Set a dark background and white text */
 .stApp {
-    background-color: #293636;      
+    background-color: #293636;
+    color: white
 }
 </style>
 """, unsafe_allow_html=True)
@@ -89,6 +90,7 @@ if show_about:
     * The algorithm weighs these features against market trends.
     * You get an evidence-based price estimate before you visit a dealer.
     """)
+
 
 
 
